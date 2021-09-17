b0b4deb218c0ec965bfb886dfcb4dd7c3c9c0a1e
shizuku <2112165916@qq.com>
2021-09-17 14:16:53 +0800

    move scripts to link.sh

---
+++ link.sh
@@ -0,0 +1,32 @@
+#!/bin/bash
+
+link() {
+  rm -rf "$2"
+  ln -s "$1" "$2"
+}
+
+
+link "$(pwd)/.zshrc" "$HOME/.zshrc"
+link "$(pwd)/.clang-format" "$HOME/.clang-format"
+link "$(pwd)/.nvimrc" "$HOME/.nvimrc"
+link "$(pwd)/.vimrc" "$HOME/.vimrc"
+link "$(pwd)/.zshrc" "$HOME/.zshrc"
+
+
+link "$(pwd)/alacritty" "$HOME/.config/alacritty"
+link "$(pwd)/i3" "$HOME/.config/i3"
+link "$(pwd)/i3status" "$HOME/.config/i3status"
+link "$(pwd)/kitty" "$HOME/.config/kitty"
+link "$(pwd)/nvim" "$HOME/.config/nvim"
+link "$(pwd)/picom" "$HOME/.config/picom"
+link "$(pwd)/polybar" "$HOME/.config/polybar"
+link "$(pwd)/ranger" "$HOME/.config/ranger"
+
+
+export CLASH_DIR="$HOME/.config/clash"
+if [ ! -e "$CLASH_DIR" ]; then
+  mkdir -p "$CLASH_DIR"
+fi
+link "$(pwd)/clash/clash.service" "$HOME/.config/clash/clash.service"
+link "$(pwd)/clash/start-clash.sh" "$HOME/.config/clash/start-clash.sh"
+link "$(pwd)/clash/stop-clash.sh" "$HOME/.config/clash/stop-clash.sh"
--- scripts/link.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-link() {
-  if [ -f "$2" ]; then
-    rm -rf "$2"
-  fi
-  ln -s "$1" "$2"
-}
