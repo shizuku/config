f82f80a216ba1597942f90e7a45abebe349d0cf1
shizuku <2112165916@qq.com>
2021-09-17 13:57:31 +0800

    modify scripts

--- scripts/link_clash.sh
+++ scripts/link_clash.sh
@@ -7,6 +7,8 @@ if [ ! -f "$clash_dir" ]; then
   mkdir -p "$clash_dir"
 fi
 
-link "$(pwd)/clash/clash.service" "$HOME/.config/clash/clash.service"
-link "$(pwd)/clash/start-clash.sh" "$HOME/.config/clash/start-clash.sh"
-link "$(pwd)/clash/stop-clash.sh" "$HOME/.config/clash/stop-clash.sh"
+prj = ".."
+
+link "$prj/clash/clash.service" "$HOME/.config/clash/clash.service"
+link "$prj/clash/start-clash.sh" "$HOME/.config/clash/start-clash.sh"
+link "$prj/clash/stop-clash.sh" "$HOME/.config/clash/stop-clash.sh"
